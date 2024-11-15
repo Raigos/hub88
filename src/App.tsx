beff236159b470d3bfeb7ac90ddd37f4a34b8d6c
Raigo Tuulik <raigo.tuulik@gmail.com>
2024-11-16 01:23:21 +0200

    Remove unnecessary container

--- src/App.tsx
+++ src/App.tsx
@@ -10,37 +10,35 @@ const App = () => {
 
 
   return (
-    <>
-      <Container>
-        <Paper sx={{ p: 2 }}>
-          {loading ? (
-            <Box display="flex" justifyContent="center">
-              <CircularProgress />
-            </Box>
-          ) : error ? (
-            <Box display="flex" justifyContent="center" color="error.main">
-              <Typography color="error" variant="body2">
-                
-                Error: {error.message}
-              </Typography>
-            </Box>
-          ) : (
-            <>
-              <TextFields />
-              <Divider />
-              <DataTable />
-            </>
-          )}
-          {/*<ul>*/}
-          {/*  {countries.map(country => (*/}
-          {/*    <li key={country.code}>*/}
-          {/*      {country.name} ({country.code})*/}
-          {/*    </li>*/}
-          {/*  ))}*/}
-          {/*</ul>*/}
-        </Paper>
-      </Container>
-    </>
+    <Container>
+      <Paper sx={{ p: 2 }}>
+        {loading ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : error ? (
+          <Box display="flex" justifyContent="center" color="error.main">
+            <Typography color="error" variant="body2">
+
+              Error: {error.message}
+            </Typography>
+          </Box>
+        ) : (
+          <>
+            <TextFields />
+            <Divider />
+            <DataTable />
+          </>
+        )}
+        {/*<ul>*/}
+        {/*  {countries.map(country => (*/}
+        {/*    <li key={country.code}>*/}
+        {/*      {country.name} ({country.code})*/}
+        {/*    </li>*/}
+        {/*  ))}*/}
+        {/*</ul>*/}
+      </Paper>
+    </Container>
   )
 }
 
